33c926caae2f8c0cd82aa4a5a48bb6675909fce6
rn-vn <taka0102rin@gmail.com>
2023-06-20 17:35:08 +0900

    add pic

--- index.js
+++ index.js
@@ -5,3 +5,15 @@
  * JS：最初の画面にiTLの写真を移り変わり形式で載せる
  */
 
+var img = new Array("images/正面玄関.jpg")
+
+var element = document.getElementById("slideshow");
+var count = -1;
+imgTimer();
+
+function imgTimer() {
+  count++;
+  if(count == img.length) count = 0;
+  element.src = img[count];
+  setTimeout("imgTimer()",2000);
+}
