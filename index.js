28ff1e282ccb9dcb73003033f6514ccf837efba6
rn-vn <taka0102rin@gmail.com>
2023-06-20 18:08:47 +0900

    complete

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-'use strict';
+"use strict";
 
 /*
  * 動画の埋め込み：iTL案内
  * JS：最初の画面にiTLの写真を移り変わり形式で載せる
  */
 
-var img = new Array("images/正面玄関.jpg")
+var img = new Array(
+  "images/キャンパス概観.jpg",
+  "images/正面玄関.jpg",
+  "images/1階エントランスホール.jpg",
+  "images/1階コミュニケーションホール.jpg",
+  "images/図書館ラーニングコモンズ.jpg",
+  "images/教室フロア.jpg",
+);
 
 var element = document.getElementById("slideshow");
 var count = -1;
@@ -13,7 +20,7 @@ imgTimer();
 
 function imgTimer() {
   count++;
-  if(count == img.length) count = 0;
+  if (count == img.length) count = 0;
   element.src = img[count];
-  setTimeout("imgTimer()",2000);
+  setTimeout("imgTimer()", 4000);
 }
